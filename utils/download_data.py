4f6e05b7d3d120812252425d553fcac78b14302f
Eli Cutler <cutler.eli@gmail.com>
2019-09-22 22:09:16 -0700

    c

--- utils/download_data.py
+++ utils/download_data.py
@@ -4,12 +4,15 @@ import urllib
 import json
 import os
 import sys; sys.path.insert(0, '..')
+
 from pathlib import Path
+from pprint import pprint
 
 from credentials import api_token
 
-query = 'dateAdded:* AND propertyType:(Home)'
-num_records = 50
+query = 'statuses.type:(For Sale OR Sold)'
+
+num_records = 1
 download = True
 data_path = Path('../data')
 
@@ -32,7 +35,7 @@ post_resp = post_req.json()
 download_id = post_resp['id']
 
 get_req = requests.get(
-    'https://api.datafiniti.co/v4/downloads' + str(download_id),
+    'https://api.datafiniti.co/v4/downloads/' + str(download_id),
     headers=request_headers
 )
 download_resp = get_req.json() 
@@ -58,5 +61,13 @@ for rec_name in rec_files:
     with open(data_path/rec_name, 'r') as rec_file:
         rec = {rec_name: json.load(rec_file)}
         rec_dict.update(rec)
+        
+with open(data_path/'11460_rec_0.json', 'r') as sample_file:
+    sample = json.load(sample_file)
 
-sample = rec_dict[list(rec_dict.keys())[0]]
+# is_sale_flattened = []
+# for r in rec_dict.values():
+#     if 'prices' in r.keys():
+#         for i in range(len(r['prices'])):
+#             if 'isSale' in r['prices'][i].keys():
+#                 is_sale_flattened.append(r['prices'][i]['isSale'])
