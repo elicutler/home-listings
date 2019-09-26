a2888a403da66dde9a7f0465bacaa0306be0c423
Eli Cutler <cutler.eli@gmail.com>
2019-09-26 04:21:43 +0000

    c

--- init_env.sh
+++ init_env.sh
@@ -1,4 +1,7 @@
 
+# restore my custom user settings in place
+cp user-settings/* ~/
+
 # create conda env then activate it
 conda env create -f environment.yml
 sudo ln -s /home/ec2-user/anaconda3/etc/profile.d/conda.sh /etc/profile.d/conda.sh
