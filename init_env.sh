04bca5c2c88da0b2b129cf306d662e84988ad74f
Eli Cutler <cutler.eli@gmail.com>
2019-09-26 04:29:41 +0000

    c

--- init_env.sh
+++ init_env.sh
@@ -1,6 +1,7 @@
 
 # restore my custom user settings in place
-cp user-settings/* ~/
+cp -rf user-settings/.ipython ~/
+cp -rf user-settings/.jupyter/lab/user-settings ~/.jupyter/lab/
 
 # create conda env then activate it
 conda env create -f environment.yml
