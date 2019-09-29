ce07d7b585f5bc0c24a397fd0bdc0ecbc942fa4b
Eli Cutler <cutler.eli@gmail.com>
2019-09-29 21:53:18 +0000

    c

--- utils/json_listing_parser.py
+++ utils/json_listing_parser.py
@@ -107,7 +107,9 @@ class JsonListingParser:
             lot_size = float(self.json_listing['lotSizeValue'])
         self.attributes['lot_size'] = lot_size
         
-    def _loop_over_features_list(features:Union[list, tuple, str]) -> object:
+    def _loop_over_features_list(
+        features:Union[list, tuple, str], mode:str='pass_val_only'
+    ) -> object:
         features_grp = features if type(features) in [list, tuple] else [features]
         def decorator(method):
             @wraps(method)
@@ -115,127 +117,49 @@ class JsonListingParser:
                 for i in range(len(self.json_listing['features'])):
                     feat_key = sample['features'][i]['key']
                     feat_val = sample['features'][i]['value']
-                    if feat_key in features_grp:
-                        method(self, feat_key, feat_val)
-                        break
+                    
+                    if mode == 'pass_val_only"'
+                        if self.clean_string(feat_key) in features_grp:
+                            method(self, feat_val)
+                            break
+                    elif mode == 'pass_key_and_val':
+                        if self.clean_string(feat_key) in features_grp:
+                            method(self, feat_val, feat_key)
+                            break
             return wrapper 
         return decorator
     
     @_loop_over_features_list(['built', 'year_built'])
-    def get_year_built(feat_key, feat_val):
+    def get_year_built(feat_val:str) -> None:
         self.attributes['year_built'] = int(feat_val[0])
         
-    
-class A:
-    attributes = {}
-    def loop_over_features(feature):
-        actual_feature = feature
-        def decorator(func):
-            @wraps(func)
-            def wrapper(self):
-                for i in range(len(sample['features'])):
-                    sample_key = sample['features'][i]['key']
-                    sample_val = sample['features'][i]['value']
-                    print(sample_key)
-                    if sample_key == actual_feature:
-                        func(self, sample_key, sample_val)
-                        break
-            return wrapper 
-        return decorator
+    @_loop_over_features_list('exterior')
+    def get_exterior(feat_val:str) -> None:
+        self.attributes['exterior'] = self.clean_string(feat_val[0])
+        
+    @_loop_over_features_list('heating_fuel')
+    def get_heating_fuel(feat_val:str) -> None:
+        self.attributes['heating_fuel'] = self.clean_string(feat_val[0])
+        
+    @_loop_over_features_list(['rooms', 'room_information'], mode='pass_key_and_val')
+    def get_num_rooms(feat_val:str, feat_key:str) -> None:
+        if self.clean_string(feat_key) == 'rooms':
+            num_rooms = self.clean_string(feat_val[0])
+        elif self.clean_string(feat_key) == 'room_information':
+            num_rooms = len(sample_value[1].split(','))
+        self.attributes['num_rooms'] = num_rooms
+        
 
-    @loop_over_features('Built')
-    def get_year_built(self, k, v):
-        year_built = int(v[0])
-        self.attributes['year_built'] = year_built
-        print(self.attributes)
-a = A()
-a.get_year_built()
-    
+        
+  
             
         
-        
-        
-        
 
-    #TODO
-################################################################################################
-
-# property history (list), which should contain one element with 
-# first listed date and first listed price, and another element with
-# closing date and closing price
-# first_listed_date = None
-# first_listed_price = None
-# first_sold_date = None
-# first_sold_price = None
-# for i in range(len(sample['features'])):
-#     if sample['features'][i]['key'] == 'Property History':
-#         if 'value' in sample['features'][i].keys():
-#             listings_with_price = [j for j in sample['features'][i]['value'] if '$' in j]
-#             for k in range(len(listings_with_price)):
-#                 rec = listings_with_price[k]
-#                 update_dt_str = re.findall(r'^.*?(?= \()', rec)[0]
-#                 update_dt = datetime.strptime(update_dt_str, '%a %b %d %Y %H:%M:%S GMT%z')
-#                 price = extract_price(rec)
-#                 if 'Sold' in rec:
-#                     if first_sold_date is None or update_dt < first_sold_date:
-#                         first_sold_date = update_dt
-#                         first_sold_price = price
-#                 elif 'Listed' in rec:
-#                     if first_listed_date is None or update_dt < first_listed_date:
-#                         first_listed_date = update_dt
-#                         first_listed_price = price
-                
-#                 print(datetime_str)t
-
-
-# first description
-# first_desc_date = None
-# first_desc = None
-# if 'descriptions' in sample.keys():
-#     for i in range(len(sample['descriptions'])):
-#         if 'dateSeen' in sample['descriptions'][i].keys():
-#             date_seen_str = sample['descriptions'][i]['dateSeen']
-#             date_seen = datetime.strptime(date_seen_str, '%Y-%m-%dT%H:%M:%S.%fZ')
-#             if first_desc_date is None or date_seen < first_desc_date:
-#                 first_desc_date = date_seen
-#                 first_desc = sample['descriptions'][i]['value']
-
-            
-# # first image
-# first_image = sample['imageURLs'][0]
-
-# other features
-
-# def clean_string(string):
-#     cleaned_string = re.sub(r'^\W+|\W+$', '', string)
-#     cleaned_string = re.sub(r'(?<!^)\W+|\W+(?=$)', '_', cleaned_string)
-#     cleaned_string = cleaned_string.lower()
-#     return cleaned_string
-
-# def extract_price(string):
-#     price_str = re.findall(r'(?<=\$)(\d+,\d+)', string)[0]
-#     price = int(price_str.replace(',', ''))
-#     return price
-
-features = {}
-features['latitude'] = float(sample['latitude'])
-features['longitude'] = float(sample['longitude'])
-features['floor_size'] = sample['floorSizeValue'] if sample['floorSizeUnit'] == 'sq. ft.' else None
-features['lot_size'] = sample['lotSizeValue'] if sample['lotSizeUnit'] == 'sq. ft.' else None
-features['property_type'] = sample['propertyType']
-for i in range(len(sample['features'])):
-    sample_key = sample['features'][i]['key']
-    sample_value = sample['features'][i]['value']
-    if clean_string(sample_key) in ('built', 'year_built'):
-        features['year_built'] = int(sample_value[0])
-    if clean_string(sample_key) == 'exterior':
-        features['exterior'] = clean_string(sample_value[0])
-    if clean_string(sample_key) == 'heating_fuel':
-        features['heating_fuel'] = clean_string(sample_value[0])
     if clean_string(sample_key) == 'rooms':
         features['num_rooms'] = int(sample_value[0])
     elif clean_string(sample_key) == 'room_information':
         features['num_rooms'] = len(sample_value[1].split(','))
+        
     if clean_string(sample_key) == 'cooling_system':
         features['cooling_system'] = clean_string(sample_value[0])
     if clean_string(sample_key) == 'taxable_value':
