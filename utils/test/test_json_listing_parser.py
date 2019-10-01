46f6665f63fc219414f31d171028c5142547487b
Eli Cutler <cutler.eli@gmail.com>
2019-10-01 05:19:02 +0000

    started unittest for json_listing_parser

--- utils/test/test_json_listing_parser.py
+++ utils/test/test_json_listing_parser.py
@@ -1,10 +1,20 @@
 
+import unittest
 import logging
 import sys; 
 sys.path.insert(0, '..')
 sys.path.insert(0, '../..')
 
+from logging_utils import set_logger_defaults
 from json_listing_parser import JsonListingParser
 
-json_listing_parser = JsonListingParser('../../data/test/0_0.json')
+logger = logging.getLogger(__name__)
+set_logger_defaults(logger)
 
+
+class TestJsonListingParser(unittest.TestCase):
+    
+    json_listing_parser = JsonListingParser('../../data/test/0_0.json')
+
+    def test_set_listing_history(self):
+        pass
