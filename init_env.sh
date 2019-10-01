bc203f647b46c96243e0c606c0c6d475297301ff
Eli Cutler <cutler.eli@gmail.com>
2019-10-01 00:34:44 +0000

    reworked jupyterlab settings in init.sh

--- init_env.sh
+++ init_env.sh
@@ -4,7 +4,7 @@ git config --global user.name 'Eli Cutler'
 git config --global user.email cutler.eli@gmail.com
 
 cp -rf user-settings/.ipython ~/
-cp -rf user-settings/.jupyter/lab/user-settings ~/.jupyter/lab/
+cp -rf user-settings/.jupyter/ ~/.jupyter/
 
 conda env create -f environment.yml
 
