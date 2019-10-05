2a709d2ec71e91840fe2890756d059ee31b3f4d4
Eli Cutler <cutler.eli@gmail.com>
2019-10-05 02:23:42 +0000

    install conda, pillow

--- data/test/img_processor.py
+++ data/test/img_processor.py
@@ -1,10 +1,11 @@
 
 import urllib
-import cv2
+from PIL import Image
 
 img_url = 'http://site.meishij.net/r/58/25/3568808/a3568808_142682562777944.jpg'
 img_local = 'img.jpg'
+
 urllib.request.urlretrieve(img_url, img_local)
 
-img = cv2.imread(img_local)
-cv2.imshow('window', img)
+img= Image.open(img_local)
+img.show() # may not work if utility not installed
