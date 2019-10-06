722d09a4c807cd615e6c67e3a24c4190a1cbd05e
Eli Cutler <cutler.eli@gmail.com>
2019-10-06 06:37:24 +0000

    started moving image encoder decoder to separate module. need to add decoder step and replace in json_listing_parser. then, modify datafiniti_downloader to have num_records and query be method args instead of class attributes

--- utils/image_coder.py
+++ utils/image_coder.py
@@ -3,6 +3,8 @@ import logging
 import urllib.request
 import ast
 import numpy as np
+import pandas as pd
+import ast
 
 from typing import Union
 from pathlib import Path
@@ -11,3 +13,22 @@ from gen_utils import set_logger_defaults
 
 logger = logging.getLogger(__name__)
 set_logger_defaults(logger)
+
+
+class ImageCoder:
+    '''
+    Download image, convert image to list representation,
+    reconstruct image.
+    '''
+        
+    @staticmethod
+    def img_url_to_arr_list(
+        url:str, local_path:Union[str, Path]
+    ) -> list:
+        urllib.request.urlretrieve(url, local_path)
+        img = Image.open(img_local) 
+        img_arr = np.asarray(img)
+        img_arr_list = img_arr.tolist()
+        return img_arr_list
+    
+    
