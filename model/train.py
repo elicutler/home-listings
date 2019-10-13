ce892770e286d8857c8bfcd5c2537f7ecdf24a97
Eli Cutler <cutler.eli@gmail.com>
2019-10-13 23:02:31 +0000

    c

--- model/train.py
+++ model/train.py
@@ -18,12 +18,12 @@ set_logger_defaults(logger)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--output_data_dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
-    parser.add_argument('--model_dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--data_dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    parser.add_argument('--output_data_dir', '-o', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument('--model_dir', '-m', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--data_dir', '-d', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
     
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch_size', '-b', type=int, default=64)
+    parser.add_argument('--epochs', '-e', type=int, default=10)
     
     args = vars(parser.parse_args())
     
@@ -35,18 +35,18 @@ if __name__ == '__main__':
     x_tab_input_dim, x_text_input_dim, x_img_input_dim = (
         trainer.get_input_dims(which_loader='train')
     )
-    trainer.set_model(
-        model=PyTorchModel(x_tab_input_dim), loss_func_cls=torch.nn.L1Loss,
-        optimizer_cls=torch.optim.Adam
-    )
-    trainer.train(epochs=5)
+    model = PyTorchModel(x_tab_input_dim)
     
-    model_path = Path(args['model_dir'])
-    state_dict = trainer.model.cpu().state_dict()
+    model_params_path = Path(args['model_dir'])/'model_params.pt'
+            
+    trainer.set_model(
+        model=model, loss_func_cls=torch.nn.L1Loss, optimizer_cls=torch.optim.Adam
+    )
+    trainer.train(epochs=args['epochs'])
        
-    with open(model_path/'model_params.pt', 'wb') as file:
-        torch.save(state_dict, file)
-        logger.info(f'Model state_dict saved to {model_path}/model_params.pt')
+    with open(model_params_path, 'wb') as file:
+        torch.save(trainer.model.cpu().state_dict(), file)
+        logger.info(f'Model state_dict saved to {model_params_path}')
 
 
 # def model_fn(model_dir:Union[Path, str]) -> PyTorchModel:
